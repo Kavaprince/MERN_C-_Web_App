1a4c47e3c84a0b7d5aabc718d8bfa5756bb7cf97
Kavaprince <davidpalavi1502@gmail.com>
2025-04-07 21:41:25 +1200

    frontend API BASE URL Updated

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:3000"; // No /api prefix
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL; // No /api prefix
 
 // Create an Axios instance
 const api = axios.create({
